49c1be465218a88e5118c99d2c05e49ec172a5b7
randxie <randxiexyy29@gmail.com>
2024-11-06 10:17:14 -0800

    flatten class

--- src/f5_tts/model/modules.py
+++ src/f5_tts/model/modules.py
@@ -357,7 +357,6 @@ class FeedForward(nn.Module):
 class Attention(nn.Module):
     def __init__(
         self,
-        processor: JointAttnProcessor | AttnProcessor,
         dim: int,
         heads: int = 8,
         dim_head: int = 64,
@@ -369,8 +368,6 @@ class Attention(nn.Module):
 
         if not hasattr(F, "scaled_dot_product_attention"):
             raise ImportError("Attention equires PyTorch 2.0, to use it, please upgrade PyTorch to 2.0.")
-
-        self.processor = processor
 
         self.dim = dim
         self.heads = heads
@@ -410,73 +407,68 @@ class Attention(nn.Module):
         rope=None,  # rotary position embedding for x
         c_rope=None,  # rotary position embedding for c
     ) -> torch.Tensor:
-        if c is not None:
-            return self.processor(self, x, c=c, mask=mask, rope=rope, c_rope=c_rope)
-        else:
-            return self.processor(self, x, mask=mask, rope=rope)
+        # if c is not None:
+        #     return self.processor(self, x, c=c, mask=mask, rope=rope, c_rope=c_rope)
+        # else:
+        return process_attn(self, x, mask=mask, rope=rope)
 
 
 # Attention processor
 
 
-class AttnProcessor:
-    def __init__(self):
-        pass
-
     # Args
         # x: float["b n d"],  # noised input x  # noqa: F722
         # mask: bool["b n"] | None = None,  # noqa: F722
-    def __call__(
-        self,
-        attn: Attention,
-        x: torch.Tensor,  # noised input x  # noqa: F722
-        mask: torch.Tensor | None = None,  # noqa: F722
-        rope=None,  # rotary position embedding
-    ) -> torch.FloatTensor:
-        batch_size = x.shape[0]
+def process_attn(
+    attn: Attention,
+    x: torch.Tensor,  # noised input x  # noqa: F722
+    mask: torch.Tensor | None = None,  # noqa: F722
+    rope=None,  # rotary position embedding
+) -> torch.FloatTensor:
+    batch_size = x.shape[0]
 
-        # `sample` projections.
-        query = attn.to_q(x)
-        key = attn.to_k(x)
-        value = attn.to_v(x)
+    # `sample` projections.
+    query = attn.to_q(x)
+    key = attn.to_k(x)
+    value = attn.to_v(x)
 
-        # apply rotary position embedding
-        if rope is not None:
-            freqs, xpos_scale = rope
-            q_xpos_scale, k_xpos_scale = (xpos_scale, xpos_scale**-1.0) if xpos_scale is not None else (1.0, 1.0)
+    # apply rotary position embedding
+    if rope is not None:
+        freqs, xpos_scale = rope
+        q_xpos_scale, k_xpos_scale = (xpos_scale, xpos_scale**-1.0) if xpos_scale is not None else (1.0, 1.0)
 
-            query = apply_rotary_pos_emb(query, freqs, q_xpos_scale)
-            key = apply_rotary_pos_emb(key, freqs, k_xpos_scale)
+        query = apply_rotary_pos_emb(query, freqs, q_xpos_scale)
+        key = apply_rotary_pos_emb(key, freqs, k_xpos_scale)
 
-        # attention
-        inner_dim = key.shape[-1]
-        head_dim = inner_dim // attn.heads
-        query = query.view(batch_size, -1, attn.heads, head_dim).transpose(1, 2)
-        key = key.view(batch_size, -1, attn.heads, head_dim).transpose(1, 2)
-        value = value.view(batch_size, -1, attn.heads, head_dim).transpose(1, 2)
+    # attention
+    inner_dim = key.shape[-1]
+    head_dim = inner_dim // attn.heads
+    query = query.view(batch_size, -1, attn.heads, head_dim).transpose(1, 2)
+    key = key.view(batch_size, -1, attn.heads, head_dim).transpose(1, 2)
+    value = value.view(batch_size, -1, attn.heads, head_dim).transpose(1, 2)
 
-        # mask. e.g. inference got a batch with different target durations, mask out the padding
-        if mask is not None:
-            attn_mask = mask
-            attn_mask = attn_mask.unsqueeze(1).unsqueeze(1)  # 'b n -> b 1 1 n'
-            attn_mask = attn_mask.expand(batch_size, attn.heads, query.shape[-2], key.shape[-2])
-        else:
-            attn_mask = None
+    # mask. e.g. inference got a batch with different target durations, mask out the padding
+    if mask is not None:
+        attn_mask = mask
+        attn_mask = attn_mask.unsqueeze(1).unsqueeze(1)  # 'b n -> b 1 1 n'
+        attn_mask = attn_mask.expand(batch_size, attn.heads, query.shape[-2], key.shape[-2])
+    else:
+        attn_mask = None
 
-        x = F.scaled_dot_product_attention(query, key, value, attn_mask=attn_mask, dropout_p=0.0, is_causal=False)
-        x = x.transpose(1, 2).reshape(batch_size, -1, attn.heads * head_dim)
-        x = x.to(query.dtype)
+    x = F.scaled_dot_product_attention(query, key, value, attn_mask=attn_mask, dropout_p=0.0, is_causal=False)
+    x = x.transpose(1, 2).reshape(batch_size, -1, attn.heads * head_dim)
+    x = x.to(query.dtype)
 
-        # linear proj
-        x = attn.to_out[0](x)
-        # dropout
-        x = attn.to_out[1](x)
+    # linear proj
+    x = attn.to_out[0](x)
+    # dropout
+    x = attn.to_out[1](x)
 
-        if mask is not None:
-            mask = mask.unsqueeze(-1)
-            x = x.masked_fill(~mask, 0.0)
+    if mask is not None:
+        mask = mask.unsqueeze(-1)
+        x = x.masked_fill(~mask, 0.0)
 
-        return x
+    return x
 
 
 # Joint Attention processor for MM-DiT
@@ -575,7 +567,6 @@ class DiTBlock(nn.Module):
 
         self.attn_norm = AdaLayerNormZero(dim)
         self.attn = Attention(
-            processor=AttnProcessor(),
             dim=dim,
             heads=heads,
             dim_head=dim_head,
